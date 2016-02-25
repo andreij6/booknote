ce7ec2ef098ab3bfc90b1890083885b34a726423
andreij6 <andreij6@gmail.com>
2016-02-24 23:32:51 -0500

    Setup Note Controller & ShelfAPI controller

---
+++ src/BookNote/Controllers/NoteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookNote.Services.ApplicationServices;
+using Microsoft.AspNet.Mvc;
+
+namespace BookNote.Controllers
+{
+	public class NoteController : Controller
+	{
+
+		public NoteController()
+		{
+		}
+
+	
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+
+	
+	}
+}
--- src/BookNote/Controllers/ShelfController.cs
+++ src/BookNote/Controllers/ShelfController.cs
@@ -5,33 +5,52 @@ using System.Threading.Tasks;
 using BookNote.Services.ApplicationServices;
 using Microsoft.AspNet.Mvc;
 
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace BookNote.Controllers
 {
+	[Route("api/[controller]")]
 	public class ShelfController : Controller
 	{
-		IBookShelfService Service;
+		private readonly IBookShelfService Service;
 
 		public ShelfController(IBookShelfService service)
 		{
 			Service = service;
 		}
 
-		//Book Shelf
-		public IActionResult Index()
+		/*
+		// GET: api/values
+		[HttpGet]
+		public IEnumerable<string> Get()
 		{
-			//var books = Service.GetBookShelf();
-
-			return View();
+			return new string[] { "value1", "value2" };
 		}
 
-		//Book Shelf Get Book
-		public IActionResult Book(int bookid)
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public string Get(int id)
 		{
-			//var book = Service.GetBook(bookid);
-
-			return View();
+			return "value";
 		}
 
-	
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+		*/
 	}
 }
