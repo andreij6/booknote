5f7709135e052f62d8a3bbe01864a24158d4b59c
andreij6 <andreij6@gmail.com>
2016-02-29 23:56:03 -0500

    feat: React
    Got a simple .jsx file connected to the view and it updates the UI. Yeeppiii!!
    
    My React Hello World

---
+++ src/BookNote/wwwroot/js/HelloWorld.jsx
@@ -0,0 +1,13 @@
+﻿// /Scripts/HelloWorld.jsx
+var HelloWorld = React.createClass({
+	render: function () {
+		return (
+		    <div>Hello {this.props.name}</div>
+        );
+}
+});
+
+ReactDOM.render(
+  <HelloWorld name="Andre"/>,
+  document.getElementById('content')
+);
